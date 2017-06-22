20ad4d688ac3c2a4c874f99e740f30923cb315b2
Jamie Doyle <thedoyles922@users.noreply.github.com>
2017-06-22 14:48:30 -0400

    installed body-parser, set up boilerplate

---
+++ app.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var mustacheExpress = require('mustache-express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var session = require('express-session');
+var parseurl = require('parseurl');
+
+var app = express();
+
+var users = [
+  {'username': 'Batman', 'password': 'supermansucks'},
+  {'username': 'Superman', 'password': 'batmansucks'},
+  {'username': 'Aquaman', 'password': 'isuck'}
+];
+
+app.engine('mustache', mustacheExpress());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'mustache');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}));
+
+
+
+
+
+
+app.listen(3000, function() {
+  console.log('listening');
+});
